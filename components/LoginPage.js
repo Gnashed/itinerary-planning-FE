c29eb4e3331d85606704e4f92c0a9315d5ed487c
Tion Blackmon <tionblackmon@icloud.com>
2024-01-30 19:41:15 -0600

    Basic skeleton

---
+++ components/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default function Page() {
+  return (
+    <>
+      <h1>We Put the IT in Itinerary</h1>
+      <Button variant="primary">Primary</Button>
+    </>
+  );
+}
