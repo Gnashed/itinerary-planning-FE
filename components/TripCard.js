c823a6222669fa8fb3d7cf03dd5d3fc3663e0a81
Tion Blackmon <tionblackmon@icloud.com>
2024-02-01 21:00:50 -0600

    Added proptypes for All Trips page (will debug, get help).

--- components/TripCard.js
+++ components/TripCard.js
@@ -1,16 +1,42 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+
+// TODO: Continue debugging.
+// export default function TripCard({ name, destination, user, startDate, endDate }) {
+//   return (
+//     <Card style={{ width: '22rem' }}>
+//       <Card.Header>{name}</Card.Header>
+//       <Card.Body>
+//         <p>{destination}</p>
+//         <p>Start Date:</p>
+//         <p>End Date:</p>
+//       </Card.Body>
+//       <Card.Footer>{user}</Card.Footer>
+//     </Card>
+//   );
+// }
+// // TODO: Continue debugging
+// TripCard.propTypes = {
+//   tripObj: PropTypes.shape({
+//     name: PropTypes.string,
+//     destination: PropTypes.string,
+//     user: PropTypes.string,
+//     startDate: PropTypes.string,
+//     endDate: PropTypes.string,
+//   }).isRequired,
+// };
 
 export default function TripCard() {
   return (
     <Card style={{ width: '22rem' }}>
-      <Card.Header>Name</Card.Header>
+      <Card.Header>name</Card.Header>
       <Card.Body>
-        <p>Destination</p>
+        <p>destination</p>
         <p>Start Date:</p>
         <p>End Date:</p>
       </Card.Body>
-      <Card.Footer>User</Card.Footer>
+      <Card.Footer>user</Card.Footer>
     </Card>
   );
 }
--- pages/trips.js
+++ pages/trips.js
@@ -1,3 +1,10 @@
+import TripCard from '../components/TripCard';
+
 export default function CreateTrip() {
-  return <h1>All Trips</h1>;
+  return (
+    <>
+      <h1>All Trips</h1>
+      <TripCard />
+    </>
+  );
 }
