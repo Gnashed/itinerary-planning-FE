1218a5b5e39895780e4727354b82ae676d685c8e
Tion Blackmon <tionblackmon@icloud.com>
2024-02-03 12:08:51 -0600

    Got my GET request to work (thanks Daungit add -A), got trips cards to render

--- components/TripCard.js
+++ components/TripCard.js
@@ -1,45 +1,29 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 
-// // TODO: Continue debugging.
-// export default function TripCard({ tripObj }) {
-//   return (
-//     <Card style={{ width: '22rem' }}>
-//       <Card.Header>{tripObj.name}</Card.Header>
-//       <Card.Body>
-//         <p>Destination: {tripObj.destination}</p>
-//         <p>Transportation: {tripObj.transportation}</p>
-//         <p>Start Date: {tripObj.startDate}</p>
-//         <p>End Date: {tripObj.endDate}</p>
-//       </Card.Body>
-//       <Card.Footer>{tripObj.user}</Card.Footer>
-//     </Card>
-//   );
-// }
-// // TODO: Continue debugging
-// TripCard.propTypes = {
-//   tripObj: PropTypes.shape({
-//     name: PropTypes.string,
-//     destination: PropTypes.string,
-//     transportation: PropTypes.string,
-//     user: PropTypes.string,
-//     startDate: PropTypes.string,
-//     endDate: PropTypes.string,
-//   }).isRequired,
-// };
-
-export default function TripCard() {
+export default function TripCard({ tripObj }) {
   return (
     <Card style={{ width: '22rem' }}>
-      <Card.Header>name</Card.Header>
+      <Card.Header>{tripObj.name}</Card.Header>
       <Card.Body>
-        <p>Destination</p>
-        <p>Transportation: </p>
-        <p>Start Date:</p>
-        <p>End Date:</p>
+        <p>Destination: {tripObj.destination}</p>
+        <p>Transportation: {tripObj.transportation}</p>
+        <p>Start Date: {tripObj.start_date}</p>
+        <p>End Date: {tripObj.end_date}</p>
       </Card.Body>
-      <Card.Footer>user</Card.Footer>
+      <Card.Footer>{tripObj.user}</Card.Footer>
     </Card>
   );
 }
+
+TripCard.propTypes = {
+  tripObj: PropTypes.shape({
+    name: PropTypes.string,
+    destination: PropTypes.string,
+    transportation: PropTypes.string,
+    user: PropTypes.string,
+    start_date: PropTypes.string,
+    end_date: PropTypes.string,
+  }).isRequired,
+};
--- pages/trips.js
+++ pages/trips.js
@@ -1,10 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import TripCard from '../components/TripCard';
+import getTrips from '../api/tripsData';
 
 export default function CreateTrip() {
+  const [trips, setTrips] = useState([]);
+
+  const getAllTheTrips = () => {
+    getTrips().then(setTrips);
+  };
+  useEffect(() => {
+    getAllTheTrips();
+  });
+
   return (
     <>
       <h1>All Trips</h1>
-      <TripCard />
+      {trips.map((trip) => (
+        <TripCard key={trip.destination} tripObj={trip} onUpdate={getAllTheTrips} />
+      ))}
     </>
   );
 }
