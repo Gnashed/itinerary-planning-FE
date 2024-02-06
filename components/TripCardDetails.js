e0f67357c6bdc59b0dd55f2fd531742109dc5c94
Tion Blackmon <tionblackmon@icloud.com>
2024-02-06 17:13:16 -0600

    Got the trip activity name to render.

--- components/TripCardDetails.js
+++ components/TripCardDetails.js
@@ -8,11 +8,11 @@ export default function ActivityCard({ tripObj }) {
   return (
     <Card className="bg-dark text-white">
       <Card.Img
-        src="holder.js/100px180"
+        src="https://www.omnihotels.com/blog/wp-content/uploads/2015/08/atlcnn-skyline-night.jpg"
         alt="Card image"
       />
       <Card.ImgOverlay>
-        <Card.Title>{tripObj.destination}</Card.Title>
+        <p style={{ color: 'whitesmoke' }}>{tripObj.name}</p>
       </Card.ImgOverlay>
     </Card>
   );
@@ -21,6 +21,6 @@ export default function ActivityCard({ tripObj }) {
 // Proptypes goes here...
 ActivityCard.propTypes = {
   tripObj: PropTypes.shape({
-    destination: PropTypes.string,
+    name: PropTypes.string,
   }).isRequired,
 };
