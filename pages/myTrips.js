31005022a4f57307e9ba0c7d58d79a4ec846de7b
Daun Kim <yennakim11@gmail.com>
2024-02-08 21:43:06 -0600

    added uid for user trips

--- pages/myTrips.js
+++ pages/myTrips.js
@@ -1,3 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import TripCard from '../components/TripCard';
+import { getUserTrips } from '../api/tripsData';
+
 export default function MyTrips() {
-  return <h1>My Trips</h1>;
+  const [userTrips, setUserTrips] = useState([]);
+
+  const getAllUserTrips = () => {
+    getUserTrips().then(setUserTrips);
+  };
+  useEffect(() => {
+    getAllUserTrips();
+  }, []);
+
+  return (
+    <>
+      <h1>User Trips</h1>
+      {userTrips.map((trip) => (
+        <TripCard key={trip} tripObj={trip} onUpdate={getAllUserTrips} />
+      ))}
+    </>
+  );
 }
